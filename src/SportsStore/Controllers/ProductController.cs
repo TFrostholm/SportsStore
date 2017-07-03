c47138e1a13bbf6ef7cd4ef12fdb709b89165a98
Toke Frostholm Rasmussen <tokefrostholm@gmail.com>
2017-07-03 18:54:39 +0200

    Added ProductController

---
+++ src/SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.Interfaces;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public ViewResult List() => View(_repository.Products);
+    }
+}
