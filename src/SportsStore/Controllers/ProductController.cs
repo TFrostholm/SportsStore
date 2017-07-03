e9c3424b7f4418756e53f2cff1f4a74a6b600492
Toke Frostholm Rasmussen <tokefrostholm@gmail.com>
2017-07-03 20:40:09 +0200

    Implemented pagination

--- src/SportsStore/Controllers/ProductController.cs
+++ src/SportsStore/Controllers/ProductController.cs
@@ -1,17 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models.Interfaces;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository _repository;
-
+        public int PageSize = 4;
         public ProductController(IProductRepository repo)
         {
             _repository = repo;
         }
 
-        public ViewResult List() => View(_repository.Products);
+        public ViewResult List(int page = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = _repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page -1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = _repository.Products.Count()
+                }
+            });
     }
 }
---
+++ src/SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
